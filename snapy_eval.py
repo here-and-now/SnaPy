24f97429e2739ad909195b01004492aacdc67c48
os <jochmann.maximilian@gmail.com>
2022-04-26 21:36:49 +0200

    model training and evaluation works

--- snapy_eval.py
+++ snapy_eval.py
@@ -1,31 +1,25 @@
-from stable_baselines3 import PPO
 import os
-from stable_baselines3.common.evaluation import evaluate_policy
+import time
 import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 from board_env import SnapyEnv
 
-import time
-
 
 name = 'score2'
-models_dir = f'models/{name}{int(time.time())}/'
-logdir = f'logs/{name}{int(time.time())}/'
-
-# env = SnapyEnv(rend=True, rendrate=25)
-# print(env)
-# env.reset()
+version = '820000'
 
 # model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
-from stable_baselines3.common.vec_env import DummyVecEnv
-model = PPO.load('models/score21650991374/750000', verbose=1)
+model = PPO.load(f'models/{name}/{version}', verbose=1)
 
-model.set_env(DummyVecEnv([lambda: SnapyEnv(rend=True)]))
+model.set_env(DummyVecEnv([lambda: SnapyEnv(rend=True, rendrate=10)]))
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
-# obs = env.reset()
-# for i in range(1000):
-    # action, _states = model.predict(obs)
-    # obs, rewards, dones, info = env.step(action)
+obs = model.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = model.step(action)
     # env.render()
 
